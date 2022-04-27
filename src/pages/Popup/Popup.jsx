dfa34d9d7929eaf6100826b5aa752e379b6c5d1b
Omar-macbook <omar987@hotmail.co.uk>
2022-04-27 21:48:27 +0100

    initail testing with react select and coingecko api

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
+import Select from 'react-select';
 
+const options = [
+  { value: 'BTC', label: 'Bitcoin' },
+  { value: 'ETH', label: 'Ethereum' },
+  { value: 'BNB', label: 'Binance' },
+];
 const Popup = () => {
+  const [state, setstate] = useState(true);
+  const handleClick = () => {
+    setstate(!state);
+    getTickerPrice('BTC');
+  };
+  const handleSelect = (selectedTicker) => {
+    getTickerPrice(selectedTicker);
+  };
   return (
-    <div className="App">
+    <div className={`App ${state && 'bg'}`}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
+        <p></p>
+
+        <Select
+          options={options}
+          name="ticker-select"
+          id="ticker-select"
+          onChange={(e) => handleSelect(e.value)}
+        />
+        <button onClick={handleClick}>click</button>
       </header>
     </div>
   );
 };
 
 export default Popup;
+
+const getTickerPrice = async (ticker) => {
+  if (ticker !== undefined) {
+    const res = await fetch(
+      `https://api.binance.com/api/v3/ticker/price?symbol=${ticker}USDT`
+    );
+    const data = await res.json();
+    console.log(`${data.symbol} price: $${data.price}`);
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -190,6 +190,12 @@ var options = {
       chunks: ['panel'],
       cache: false,
     }),
+    new webpack.ProvidePlugin({
+      // Make a global `process` variable that points to the `process` package,
+      // because the `util` package expects there to be a global variable named `process`.
+      // Thanks to https://stackoverflow.com/a/65018686/14239942
+      process: 'process/browser',
+    }),
   ],
   infrastructureLogging: {
     level: 'info',
