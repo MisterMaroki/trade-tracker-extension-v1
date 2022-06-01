1c99d5a460c59140358e95f2bb6fa64a907e402e
Omar-macbook <omar987@hotmail.co.uk>
2022-06-01 14:49:00 +0100

    Popup shared context disrupts the firebase auth persitence process

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,63 +1,20 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
+import '../Newtab/styles/Newtab.scss';
 import './Popup.css';
 
 import { CryptoState } from '../Newtab/CryptoContext';
 import { Select } from '@mui/material';
 import TradeTools from '../Newtab/Components/TradeTools';
-
+import AuthModal from '..//Newtab/Components/auth/AuthModal.js';
 const options = [
   { value: 'bitcoin', label: 'Bitcoin' },
   { value: 'ethereum', label: 'Ethereum' },
   { value: 'binancecoin', label: 'Binance' },
 ];
 const Popup = () => {
-  const [currentTicker, setCurrentTicker] = useState('bitcoin');
-  const [currentPrice, setCurrentPrice] = useState(
-    async () => getTickerPrice(currentTicker) || 0
-  );
-
-  const priceEl = useRef(null);
-  const getTickerPrice = async (ticker) => {
-    if (ticker !== undefined) {
-      const res = await fetch(
-        `https://api.coingecko.com/api/v3/simple/price?ids=${ticker}&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_last_updated_at=true`
-      );
-      const data = await res.json();
-      setCurrentPrice(data[Object.keys(data)[0]]?.usd);
-    }
-  };
-
-  useEffect(() => {
-    getTickerPrice(currentTicker);
-    priceEl.current.textContent = `${currentTicker.toUpperCase()}: $${parseFloat(
-      currentPrice
-    ).toFixed(2)}`;
-  }, [currentPrice, currentTicker]);
-
-  const handleSelect = (selectedTicker) => {
-    setCurrentTicker(selectedTicker);
-    getTickerPrice(selectedTicker);
-  };
-
-  const { currency } = CryptoState();
-  console.log('🚀 ~ file: Popup.jsx ~ line 45 ~ Popup ~ currency', currency);
-
-  return (
-    <div className={`__container `}>
-      <p ref={priceEl}>BTC: $39225.00</p>
-      <TradeTools />
-      <Select
-        options={options}
-        name="ticker-select"
-        id="ticker-select"
-        value={currentTicker}
-        onChange={(e) => handleSelect(e.value)}
-      />
-      <button>Buy</button>
-    </div>
-  );
+  return <div className={`__container `}>Features in progress.</div>;
 };
 
 export default Popup;
